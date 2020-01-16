3293525de141bfe24e0b3670bcfde663fec4fdeb
Matthew Britt <mwbritt@hotmail.com>
2020-01-16 00:26:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  
+  data.each do |first_key, value|
+    
+    value.each do |second_key, array|
+      
+      array.each do |pigeon_name|
+        if pigeon_list[pigeon_name] == nil 
+          pigeon_list[pigeon_name] = {}
+        end
+        
+        if pigeon_list[pigeon_name][first_key] == nil 
+          pigeon_list[pigeon_name][first_key] = []
+        end
+        
+        pigeon_list[pigeon_name][first_key].push(second_key.to_s)
+      end
+      
+    end
+    
+  end
+  
+  pigeon_list
+  
 end
