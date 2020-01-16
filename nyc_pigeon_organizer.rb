875624709823219f8ae18058d36a00cec699ae59
Matthew Britt <mwbritt@hotmail.com>
2020-01-16 00:04:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,9 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   
-  data.each do |
+  data.each do |first_order_keys, value|
+    
+    value.each do |second_order_keys, array|
+      
   
 end
