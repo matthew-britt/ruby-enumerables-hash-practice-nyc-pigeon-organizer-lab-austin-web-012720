87b61303081addb7a4ce4515d336c93229d97983
Matthew Britt <mwbritt@hotmail.com>
2020-01-16 00:06:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,15 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   
-  data.each do |first_order_keys, value|
+  data.each do |first_key, value|
     
-    value.each do |second_order_keys, array|
+    value.each do |second_key, array|
       
+      array.each do |pigeon_name|
+        if pigeon_list[pigeon_name] == nil 
+          pigeon_list[pigeon_name] == {}
+        end
+        
+        if pigeon_list[pigeon_name]
   
 end
